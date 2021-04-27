854d49a76d73404b90a77743b22f343d8a671ecf
nvtinh3001 <tinhnv@diachiso.vn>
2021-04-27 09:57:03 +0700

    đ

---
+++ Classes/File1.swift
@@ -0,0 +1,14 @@
+//
+//  File1.swift
+//  DemoChuan
+//
+//  Created by iMac on 27/04/2021.
+//
+
+import Foundation
+
+public struct File1{
+    public func show(){
+        print("File1")
+    }
+}
---
+++ Classes/File2.swift
@@ -0,0 +1,13 @@
+//
+//  File2.swift
+//  DemoChuan
+//
+//  Created by iMac on 27/04/2021.
+//
+
+import Foundation
+public struct File2{
+    public func show(){
+        print("File1")
+    }
+}
